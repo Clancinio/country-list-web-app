633c12364f9db9b0347136566da73512e900d4e2
DeanC <dean.clancy91@gmail.com>
2020-08-24 14:54:19 -0700

    Add viewHomePage method in CountryController

--- src/main/java/com/deanclancydev/countrylistwebapp/controller/CountryController.java
+++ src/main/java/com/deanclancydev/countrylistwebapp/controller/CountryController.java
@@ -1,13 +1,22 @@
 package com.deanclancydev.countrylistwebapp.controller;
 
+import com.deanclancydev.countrylistwebapp.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     // Display a list of all Countries
     // Method handler for home page (index.html) to display list of countries
-    public String viewHomePage(){
-        
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("listCountries", countryService.getAllCountries());
+        return "index";
     }
 }
