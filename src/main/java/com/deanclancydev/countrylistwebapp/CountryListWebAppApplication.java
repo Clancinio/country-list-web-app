63e3098569eaa1e3e41fb3e9b438962962e6fdbb
DeanC <dean.clancy91@gmail.com>
2020-08-24 14:03:37 -0700

    Initial Commit

---
+++ src/main/java/com/deanclancydev/countrylistwebapp/CountryListWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.deanclancydev.countrylistwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryListWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryListWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deanclancydev/countrylistwebapp/CountryListWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deanclancydev.countrylistwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryListWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
